339d34a3210e9db607b6557a745c779a77d72a95
Turbobot <turbobot@vercel.com>
2025-01-25 18:53:24 -0400

    feat(create-turbo): apply package-manager transform

--- expo-app/app/index.tsx
+++ expo-app/app/index.tsx
@@ -9,7 +9,7 @@ const index = () => {
       <VStack className="p-2 md:max-w-[440px] w-full" space="xl">
         <Button
           onPress={() => {
-            router.push("auth/splash-screen");
+            router.push("../auth/splash-screen");
           }}
         >
           <ButtonText>SplashScreen</ButtonText>
@@ -17,49 +17,49 @@ const index = () => {
         <Button
           className="w-full"
           onPress={() => {
-            router.push("auth/signin");
+            router.push("../auth/signin");
           }}
         >
           <ButtonText>Sign in</ButtonText>
         </Button>
         <Button
           onPress={() => {
-            router.push("auth/signup");
+            router.push("../auth/signup");
           }}
         >
           <ButtonText>Sign up</ButtonText>
         </Button>
         <Button
           onPress={() => {
-            router.push("auth/forgot-password");
+            router.push("../auth/forgot-password");
           }}
         >
           <ButtonText>Forgot password</ButtonText>
         </Button>
         <Button
           onPress={() => {
-            router.push("auth/create-password");
+            router.push("../auth/create-password");
           }}
         >
           <ButtonText>Create password</ButtonText>
         </Button>
         <Button
           onPress={() => {
-            router.push("news-feed/news-and-feed");
+            router.push("../news-feed/news-and-feed");
           }}
         >
           <ButtonText>News feed</ButtonText>
         </Button>
         <Button
           onPress={() => {
-            router.push("dashboard/dashboard-layout");
+            router.push("../dashboard/dashboard-layout");
           }}
         >
           <ButtonText>Dashboard</ButtonText>
         </Button>
         <Button
           onPress={() => {
-            router.push("profile/profile");
+            router.push("../profile/profile");
           }}
         >
           <ButtonText>Profile</ButtonText>
